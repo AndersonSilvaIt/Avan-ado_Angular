46c4cbae470f9d7d985e8c1697355d69bc5c8bc0
Anderson <anderson@prodwin.com.br>
2021-12-27 13:59:19 -0300

    Implementado validações simples e customizadas.
    Implementado validações simples e customizadas.

--- TemplateProjetoBasico-Formularios/src/app/app.module.ts
+++ TemplateProjetoBasico-Formularios/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
+import { NgBrazil } from 'ng-brazil'
+import { TextMask} from 'ng-brazil'
+import { CustomFormsModule } from 'ng2-validation'
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
@@ -25,6 +28,9 @@ import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.compo
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    NgBrazil,
+    TextMask.TextMaskModule,
+    CustomFormsModule,
     [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
   ],
   providers: [
--- TemplateProjetoBasico-Formularios/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ TemplateProjetoBasico-Formularios/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from './models/usuario';
+import { NgBrazilValidators } from 'ng-brazil';
+import { utilsBr } from 'js-brasil';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,23 +13,36 @@ export class CadastroComponent implements OnInit {
 
   cadastroForm : FormGroup;
   usuario: Usuario;
+  formResult: string = '';
+  MASKS = utilsBr.MASKS;
 
   constructor(private fb : FormBuilder)  { }
 
   ngOnInit() {
 
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15])]);
+    let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15]), CustomValidators.equalTo(senha)]);
+
     this.cadastroForm = this.fb.group({
-       nome: [''],
-       cpf: [''],
-       email: [''],
-       senha: [''],
-       senhaConfirmacao: ['']
+        nome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(150)]],
+       cpf: ['', [Validators.required, NgBrazilValidators.cpf]],
+       email: ['', [Validators.required, Validators.email]],
+       senha: senha,
+       senhaConfirmacao: senhaConfirm
     });
 
   }
 
   adicionarUsuario(){
-    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value)
+  
+    if(this.cadastroForm.dirty && this.cadastroForm.valid){
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+    }else{
+      this.formResult = "Não submeteu";
+    }
+
+
   }
 
 }
