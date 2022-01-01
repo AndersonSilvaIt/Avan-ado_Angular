6a9e88269c763d7d43965eeb8bb18abc9b85f2fe
Anderson <anderson@prodwin.com.br>
2022-01-01 20:18:48 -0300

    Implementado eventos de formulários filhos.
    Implementado eventos de formulários filhos.

--- TemplateProjetoBasico-Formularios/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ TemplateProjetoBasico-Formularios/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { ProdutoDetalheComponent } from '../componentes/produto-card-detalhe.component';
+import { ProdutoCountComponent } from '../componentes/produto-count.component';
 import { Produto } from '../models/produto';
 
 @Component({
@@ -6,11 +9,17 @@ import { Produto } from '../models/produto';
   templateUrl: './produto-dashboard.component.html',
   styles: []
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
   
   produtos: Produto[]
+  
+  @ViewChild(ProdutoCountComponent, {static: false}) contador: ProdutoCountComponent
+  @ViewChild('teste', { static: false}) mensagemTela : ElementRef;
+
+  @ViewChildren(ProdutoDetalheComponent) botoes : QueryList<ProdutoDetalheComponent>
 
   constructor() { }
+
 
   ngOnInit() {
 
@@ -61,6 +70,24 @@ export class ProdutoDashboardComponent implements OnInit {
 
   mudarStatus(event: Produto){
     event.ativo = !event.ativo;
+  }
+
+  ngAfterViewInit(): void {
+
+    console.log('Objeto do Contador: ', this.contador.produtos);
+    
+    let clickTexto: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click');
+  
+    clickTexto.subscribe(() => {
+      alert('Clicou no texto');
+      return;
+    });
+
+    console.log(this.botoes);
+    this.botoes.forEach(p => {
+      console.log(p.produto);
+    });
+
   }
 
 }
