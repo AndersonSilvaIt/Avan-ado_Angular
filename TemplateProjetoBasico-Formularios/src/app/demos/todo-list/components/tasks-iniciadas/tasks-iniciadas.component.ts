472cd67ba556218258de956cae0c0ad5041e1632
Anderson <anderson@prodwin.com.br>
2022-01-26 17:27:27 -0300

    Implementado o preenchimento do estado dentro da Store
    Implementado o preenchimento do estado dentro da Store

--- TemplateProjetoBasico-Formularios/src/app/demos/todo-list/components/tasks-iniciadas/tasks-iniciadas.component.ts
+++ TemplateProjetoBasico-Formularios/src/app/demos/todo-list/components/tasks-iniciadas/tasks-iniciadas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { TasksService } from "../../todo.service";
+import { Store } from "../../todo.store";
 
 @Component({
   selector: "tasks-iniciadas",
@@ -10,9 +12,12 @@ export class TasksIniciadasComponent implements OnInit {
   
   iniciados$:Observable<any[]> 
 
-  constructor(private taskService : TasksService) { }
+  constructor(private taskService : TasksService, private store: Store) { }
 
   ngOnInit() { 
-      this.iniciados$ = this.taskService.getTodoList$;
+    this.iniciados$ = this.store.getTodoList()
+      .pipe(
+        map(todolist => todolist.filter(task => task.iniciado && !task.finalizado))
+      );
   }
 }
--- TemplateProjetoBasico-Formularios/src/app/demos/todo-list/todo.service.ts
+++ TemplateProjetoBasico-Formularios/src/app/demos/todo-list/todo.service.ts
@@ -1,19 +1,28 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 import { Task } from "./tasks";
+import { Store } from "./todo.store";
 
 @Injectable()
 export class TasksService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private store: Store) { }
 
     getTodoList$: Observable<Task[]> = this.http
-        .get<Task[]>('http://localhost:3000/todolist');
+        .get<Task[]>('http://localhost:3000/todolist')
+        .pipe(
+            tap(next => this.store.set('todolist', next))
+        );
 
     // Ambos os métodos fazem a mesma coisa ... a diferença é que a de cima, fica sendo uma propriedade
     //getToDoList() : Observable<Task[]> {
     //    return this.http
     //        .get<Task[]>('http://localhost:3000/todolist')
     //}
+}
+
+function tep(tep: any): Observable<Task[]> {
+    throw new Error("Function not implemented.");
 }
